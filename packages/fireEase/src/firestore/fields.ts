916f0947aae47e3e2f0da4aba4d0e2151203f749
Omer M <omerplays1411@gmail.com>
2023-09-01 20:33:33 +0300

    Added fields.ts

---
+++ packages/fireEase/src/firestore/fields.ts
@@ -0,0 +1,27 @@
+export enum FieldTypes {
+  string = 1,
+  number = 2,
+  boolean = 3,
+  map = 4,
+  array = 5,
+  null = 6,
+  timestamp = 7,
+  geopoint = 8,
+  refrence = 9,
+}
+
+export type Field = {
+  type: FieldTypes;
+  name: string;
+};
+
+function FieldFunctionByType(type: FieldTypes): (name: string) => Field {
+  return (name: string) => {
+    return { type: type, name: name };
+  };
+}
+
+export const NullField = FieldFunctionByType(FieldTypes.null);
+export const StringField = FieldFunctionByType(FieldTypes.string);
+export const NumberField = FieldFunctionByType(FieldTypes.number);
+export const BooleanField = FieldFunctionByType(FieldTypes.boolean);
