475f0eaa063edae4139fd5c4076e65f60b24cdb1
mendlero <omerplays1411@gmail.com>
2023-09-03 13:33:22 +0300

    added collectionLocation to DBCollection class

--- packages/fireEase/src/firestore/DBCollection.ts
+++ packages/fireEase/src/firestore/DBCollection.ts
@@ -1,6 +1,8 @@
 import { FieldsInput, RelationalFieldsInput } from './fields';
 
 class DBCollection {
+  collectionLocation: string = '';
+
   fields: FieldsInput = {};
   relationalField: RelationalFieldsInput = {};
 }
