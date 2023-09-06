90ab9c2271ef25faefcc5e94ea851e896e19fb08
mendlero <omerplays1411@gmail.com>
2023-09-06 13:01:28 +0300

    allowed extending DBCollections

--- packages/fireEase/src/firestore/DBCollection.ts
+++ packages/fireEase/src/firestore/DBCollection.ts
@@ -1,13 +1,65 @@
 import { FieldProps, RelationalFieldProps } from './fields';
 import { GenericFieldsInput } from './genericFields';
 
-export interface DBCollection extends DBCollectionFields {
-  extends?: DBCollectionFields | DBCollectionFields[];
-  collectionLocation: string;
-  autoId: boolean;
+type FieldsInput = GenericFieldsInput<FieldProps>;
+type RelationalFieldsInput = GenericFieldsInput<RelationalFieldProps>;
+
+class DBCollectionFields {
+  constructor(
+    public fields: FieldsInput = {},
+    extend?: DBCollectionFields | DBCollectionFields[],
+    public relationalField: RelationalFieldsInput = {}
+  ) {
+    if (extend) {
+      if (Array.isArray(extend)) {
+        extend.forEach((CollectionFields) => {
+          this.extendWith(CollectionFields);
+        });
+      } else {
+        this.extendWith(extend);
+      }
+    }
+  }
+
+  extendWith(extend: DBCollectionFields) {
+    if (extend.fields) {
+      this.addFields(extend.fields);
+    }
+
+    if (extend.relationalField) {
+      this.addRelationalFields(extend.relationalField);
+    }
+  }
+
+  addFields(fields: FieldsInput) {
+    const exsistingFields = Object.keys(this.fields);
+    Object.entries(fields).forEach(([k, v]) => {
+      if (!(k in exsistingFields)) {
+        this.fields[k] = v;
+      }
+    });
+  }
+
+  addRelationalFields(fields: RelationalFieldsInput) {
+    const exsistingRelationalFields = Object.keys(this.relationalField);
+    Object.entries(fields).forEach(([k, v]) => {
+      if (!(k in exsistingRelationalFields)) {
+        this.relationalField[k] = v;
+      }
+    });
+  }
 }
 
-export interface DBCollectionFields {
-  fields: GenericFieldsInput<FieldProps>;
-  relationalField?: GenericFieldsInput<RelationalFieldProps>;
+export class DBCollection extends DBCollectionFields {
+  constructor(
+    public collectionLocation: string,
+    public autoId: boolean = false,
+    fields: FieldsInput = {},
+    extend?: DBCollectionFields | DBCollectionFields[],
+    relationalField: RelationalFieldsInput = {}
+  ) {
+    super(fields, extend, relationalField);
+  }
 }
+
+export class DBCollectionExtension extends DBCollectionFields {}
