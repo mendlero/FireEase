cf5f695df447017448ea492110de0b00e0914055
Omer M <omerplays1411@gmail.com>
2023-09-08 07:25:44 +0300

    added basic bin

---
+++ packages/fireEase/src/bin/index.ts
@@ -0,0 +1,7 @@
+try {
+  require.resolve('firebase');
+} catch (e) {
+  console.warn(
+    `The module 'firebase' was not found. Next.js requires that you include it in 'dependencies' of your 'package.json'. To add it, run 'npm install firebase'`
+  );
+}
